41b912c11ff405f918d50d97adc8a002912531f2
vikaskumarreddy <vikaskumarreddy.nachireddy@gmail.com>
2025-05-15 16:40:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
-window.onload = () => {
-    const ui = SwaggerUIBundle({
-        url: "http://localhost:9003/v2/api-docs",
-        dom_id: '#swagger-ui',
-        presets: [
-            SwaggerUIBundle.presets.apis,
-            SwaggerUIStandalonePreset
-        ],
-        layout: "StandaloneLayout"
-    });
+let urlLocal = 'https://api-staging.securew2.com/v1/docs/enrollment'; // default
+let bearerToken = 'YOUR_BEARER_TOKEN_HERE'; // Set your token here or dynamically
 
-    // Wait for Swagger UI to load and then modify DOM
-    setTimeout(() => {
-        const topbar = document.querySelector('.swagger-ui .topbar-wrapper .link');
-        if (topbar) {
-            topbar.insertAdjacentHTML('beforeend', '<span style="color:white; font-size:18px; margin-left:10px;">SecureW2 API Portal</span>');
-        }
-    }, 500);
-};
+function loadSwaggerUI() {
+  SwaggerUIBundle({
+    url: urlLocal,
+    dom_id: '#swagger-ui',
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    layout: "StandaloneLayout",
+    requestInterceptor: function (req) {
+      req.headers['Authorization'] = `Bearer ${bearerToken}`;
+      return req;
+    }
+  });
+}
+
+function handleApiChange(value) {
+  switch (value) {
+    case 'enrollment':
+      urlLocal = 'http://localhost:9003/v1/docs/enrollment';
+      break;
+    case 'certificate':
+      urlLocal = 'http://localhost:8006/v1/organizations/12345/certificates/v1/docs/certificate';
+      break;
+    case 'device':
+      urlLocal = 'http://localhost:8075/v1/organizations/12345/devices/v1/docs/device';
+      break;
+    case 'user':
+      urlLocal = 'http://localhost:8025/v1/organizations/12345/users/v1/docs/user';
+      break;
+  }
+  loadSwaggerUI(); // reload with new path
+}
+
+window.onload = loadSwaggerUI;
