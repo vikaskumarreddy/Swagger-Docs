c5c98ca66636fcdfee37a30d0250f589a3d5aca9
vikaskumarreddy <vikaskumarreddy.nachireddy@gmail.com>
2025-05-15 17:46:47 +0530

    Update index.js with token accepted

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let urlLocal = 'https://api-staging.securew2.com/v1/docs/enroll'; // default
+let urlLocal = 'https://api-staging.securew2.com/v1/docs/enrollment'; // default
+const bearerToken = '19k8nvgjlu1k5brfugftfdt94j'; // Replace with your actual token
 
 function loadSwaggerUI() {
   SwaggerUIBundle({
@@ -8,26 +9,30 @@ function loadSwaggerUI() {
       SwaggerUIBundle.presets.apis,
       SwaggerUIStandalonePreset
     ],
-    layout: "StandaloneLayout"
+    layout: "StandaloneLayout",
+    requestInterceptor: (req) => {
+      req.headers['Authorization'] = `Bearer ${bearerToken}`;
+      return req;
+    }
   });
 }
 
 function handleApiChange(value) {
   switch (value) {
     case 'enrollment':
-      urlLocal = 'https://api-staging.securew2.com/v1/docs/enroll';
+      urlLocal = 'https://api-staging.securew2.com/v1/docs/enrollment';
       break;
     case 'certificate':
       urlLocal = 'http://localhost:8006/v1/organizations/12345/certificates/v1/docs/certificate';
       break;
     case 'device':
-      urlLocal = 'http://localhost:8075/v1/organizations/12345/devices/v1/docs/device';
+      urlLocal = 'https://api-staging.securew2.com/v1/devices/v2/api-docs';
       break;
     case 'user':
       urlLocal = 'http://localhost:8025/v1/organizations/12345/users/v1/docs/user';
       break;
   }
-  loadSwaggerUI(); // reload with new path
+  loadSwaggerUI(); // reload with new path and token
 }
 
 window.onload = loadSwaggerUI;
