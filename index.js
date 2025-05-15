fb277f8c29ba60e40dd2782bb946befc27a2027a
vikaskumarreddy <vikaskumarreddy.nachireddy@gmail.com>
2025-05-15 16:42:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 let urlLocal = 'https://api-staging.securew2.com/v1/docs/enrollment'; // default
-let bearerToken = 'YOUR_BEARER_TOKEN_HERE'; // Set your token here or dynamically
 
 function loadSwaggerUI() {
   SwaggerUIBundle({
@@ -9,18 +8,14 @@ function loadSwaggerUI() {
       SwaggerUIBundle.presets.apis,
       SwaggerUIStandalonePreset
     ],
-    layout: "StandaloneLayout",
-    requestInterceptor: function (req) {
-      req.headers['Authorization'] = `Bearer ${bearerToken}`;
-      return req;
-    }
+    layout: "StandaloneLayout"
   });
 }
 
 function handleApiChange(value) {
   switch (value) {
     case 'enrollment':
-      urlLocal = 'http://localhost:9003/v1/docs/enrollment';
+      urlLocal = 'https://api-staging.securew2.com/v1/docs/enrollment';
       break;
     case 'certificate':
       urlLocal = 'http://localhost:8006/v1/organizations/12345/certificates/v1/docs/certificate';
